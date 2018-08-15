17087f9245fdb92ed0e2ee54d1247eff03cdf9a3
vplesheva <vplesheva@gmail.com>
2018-08-15 20:41:21 +0300

    Add files

---
+++ assets/src/js/main.js
@@ -0,0 +1,21 @@
+// Импортируем jQuery
+//= ../bower_components/jquery/dist/jquery.js
+ 
+// Импортируем Popper
+//= ../bower_components/popper.js/dist/umd/popper.js
+ 
+// Импортируем необходимые js-файлы Bootstrap 4
+//= ../bower_components/bootstrap/js/dist/util.js
+//= ../bower_components/bootstrap/js/dist/alert.js
+//= ../bower_components/bootstrap/js/dist/button.js
+//= ../bower_components/bootstrap/js/dist/carousel.js
+//= ../bower_components/bootstrap/js/dist/collapse.js
+//= ../bower_components/bootstrap/js/dist/dropdown.js
+//= ../bower_components/bootstrap/js/dist/modal.js
+//= ../bower_components/bootstrap/js/dist/scrollspy.js
+//= ../bower_components/bootstrap/js/dist/tab.js
+//= ../bower_components/bootstrap/js/dist/tooltip.js
+//= ../bower_components/bootstrap/js/dist/popover.js
+ 
+// Импортируем другие js-файлы
+//= my.js
